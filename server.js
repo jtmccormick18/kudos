02d4245e09d5bedb0b573acee987ad10bc80fcd3
Josh McCormick <josh.mccormick81@gmail.com>
2018-11-25 10:12:01 -0500

    heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static("public"));
 var url='mongodb://heroku_hgpl7kqd:o58v666g982u1s1o3kkklvoldj@ds033113.mlab.com:33113/heroku_hgpl7kqd'
 
 if(process.env.MONGODB_URI){
-  mongoose.connect(MONGODB_URI);
+  mongoose.connect(MONGODB_URI,{useNewUrlParser:true});
 } else{
   mongoose.connect('mongodb://localhost/kudos_db', { useNewUrlParser: true });
 }
