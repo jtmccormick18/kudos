0b319bb886b2aa6a066db2b9bfab4e9811e92985
Josh McCormick <josh.mccormick81@gmail.com>
2018-11-26 14:28:44 -0500

    fixed kudo push

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -22,11 +22,11 @@ module.exports = function (app) {
     app.post('/api/users', function (req, res) {
         console.log(req.body)
         Users.create(req.body)
-        .then(function (newUser) {
-            res.json(newUser)
-        }).catch(function (err) {
-            res.json(err);
-        });
+            .then(function (newUser) {
+                res.json(newUser)
+            }).catch(function (err) {
+                res.json(err);
+            });
     })
     app.post('/api/kudos', function (req, res) {
         Kudos.create({
@@ -36,15 +36,17 @@ module.exports = function (app) {
             body: req.body.body
         })
             .then(function (data) {
-                return Users.update({$or:
-                    [{ _id: req.body.getter_id},{_id:req.body.sender_id}]
-                }),
+                return Users.update({
+                    $or:
+                        [{ _id: req.body.getter_id }, { _id: req.body.sender_id }]
+                },
                     {
                         $push: {
                             kudos: data._id
                         }
 
                     }
+                )
             })
             .then(function (data) {
                 console.log(data)
